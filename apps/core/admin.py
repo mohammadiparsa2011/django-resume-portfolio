18b4aec0e41ac703ba89279be9fceffe49298282
mohammadiparsa2011 <mohammadiparsa@gmail.com>
2026-08-01 08:09:56 +0330

    feat: add dynamic Hero and Services models with admin interface

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import HeroSection, ServicesSection
 
 # Register your models here.
+
+admin.site.register(HeroSection)
+admin.site.register(ServicesSection)
--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class HeroSection(models.Model):
+    name = models.CharField(max_length=100, verbose_name='نام')
+    job_title = models.CharField(max_length=255, verbose_name='عنوان شغل')
+    button_text = models.CharField(max_length=100, verbose_name='متن دکمه')
+    avatar = models.ImageField(upload_to='hero/', verbose_name='تصویر آواتار(png)')
+    resume_file = models.FileField(upload_to='hero/resume/', verbose_name='رزومه(pdf & image)', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'ماژول اطلاعات شخصی'
+        verbose_name_plural = 'اطلاعات شخصی'
+
+class ServicesSection(models.Model):
+    title = models.CharField(max_length=100, verbose_name='عنوان توانایی')
+    description = models.CharField(max_length=300, verbose_name='توضیحات')
+    svg_code = models.TextField(verbose_name='SVG کد')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'ماژول توانایی‌ها'
+        verbose_name_plural = "توانایی‌ها"
