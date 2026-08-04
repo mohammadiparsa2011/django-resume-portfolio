5426ffe674dda7f54cc5b6c4695ad848cb5d9046
mohammadiparsa2011 <mohammadiparsa@gmail.com>
2026-08-04 08:53:21 +0330

    feat: register About, SocialLink, Skill, and SkillIcon in admin

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import HeroSection, ServicesSection
+from .models import HeroSection, ServicesSection, About, SocialLink, Skill, SkillIcon
 
 # Register your models here.
 
 admin.site.register(HeroSection)
 admin.site.register(ServicesSection)
+admin.site.register(About)
+admin.site.register(SocialLink)
+admin.site.register(Skill)
+admin.site.register(SkillIcon)
