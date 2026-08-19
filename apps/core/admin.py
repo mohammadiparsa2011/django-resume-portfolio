b95d16e86bc557ab6eea9574f42db9b538194c85
mohammadiparsa2011 <mohammadiparsa@gmail.com>
2026-08-19 10:32:09 +0330

    feat: register PortfolioCategory and PortfolioItem in admin with filter_horizontal

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import HeroSection, ServicesSection, About, SocialLink, Skill, SkillIcon, WorkExperience, EducationExperience
+from .models import HeroSection, ServicesSection, About, SocialLink, Skill, SkillIcon, WorkExperience, EducationExperience, PortfolioItem, PortfolioCategory
 
 # Register your models here.
 
@@ -11,3 +11,17 @@ admin.site.register(Skill)
 admin.site.register(SkillIcon)
 admin.site.register(WorkExperience)
 admin.site.register(EducationExperience)
+
+@admin.register(PortfolioCategory)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(PortfolioItem)
+class PortfolioItemAdmin(admin.ModelAdmin):
+    list_display = ['title', 'display_categories']
+    filter_horizontal = ['categories']  # ✅ این باعث میشه انتخاب چند دسته‌بندی توی ادمین راحت باشه
+
+    def display_categories(self, obj):
+        return ", ".join([cat.name for cat in obj.categories.all()])
+    display_categories.short_description = 'دسته‌بندی‌ها'
