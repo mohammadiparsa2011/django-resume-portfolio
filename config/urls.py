a2e3ccf3d6c02db4b5b47efcbce49704f99a2a01
mohammadiparsa2011 <mohammadiparsa@gmail.com>
2026-09-21 13:33:43 +0330

    fix: add namespace='core' to core urls include

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,4 @@
+# config/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -5,10 +6,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.core.urls')),  # مسیر خالی به core
+    path('', include('apps.core.urls', namespace='core')),
 ]
 
-# در حالت توسعه (DEBUG=True) فایل‌های رسانه را سرو کنیم
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # اختیاری
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
